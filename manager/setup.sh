bade396447584a9ab4299af431dcc1f481e11dde
Nick Kotenberg <nkotenberg@fusionware.com>
2018-11-05 17:33:50 -0700

    f

--- manager/setup.sh
+++ manager/setup.sh
@@ -21,10 +21,8 @@ cp ./orderer/orderer.genesis.block crypto-config/ordererOrganizations/segfault.c
 cp ./orderer/orderer.genesis.block crypto-config/ordererOrganizations/segfault.com/orderers/orderer1.segfault.com
 cp ./orderer/orderer.genesis.block crypto-config/ordererOrganizations/segfault.com/orderers/orderer2.segfault.com
 
-# mkdir -p /storage/orderer1
-# mkdir -p /storage/orderer2
-# mkdir -p /storage/orderer3
+mkdir -p /storage/orderer1
+mkdir -p /storage/orderer2
+mkdir -p /storage/orderer3
 
-# cp -R crypto-config/ordererOrganizations/segfault.com/orderers/orderer0.segfault.com /storage/orderer1
-# cp -R crypto-config/ordererOrganizations/segfault.com/orderers/orderer1.segfault.com /storage/orderer2
-# cp -R crypto-config/ordererOrganizations/segfault.com/orderers/orderer2.segfault.com /storage/orderer3
+cp -R crypto-config/ordererOrganizations/segfault.com/orderers /storage
