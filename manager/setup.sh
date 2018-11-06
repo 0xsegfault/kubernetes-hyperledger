93e20c9e3645daf0c7442e543bcf1b46031cee81
Nick Kotenberg <nkotenberg@fusionware.com>
2018-11-05 17:25:06 -0700

    added last part for

--- manager/setup.sh
+++ manager/setup.sh
@@ -16,3 +16,15 @@ mkdir /app/channels
 ./fabric-samples/bin/configtxgen -profile MainChannel -outputAnchorPeersUpdate ./channels/mainchannelorg1anchor.tx -channelID mainchannel -asOrg Org1MSP
 ./fabric-samples/bin/configtxgen -profile MainChannel -outputAnchorPeersUpdate ./channels/mainchannelorg2anchor.tx -channelID mainchannel -asOrg Org2MSP
 ./fabric-samples/bin/configtxgen -profile MainChannel -outputAnchorPeersUpdate ./channels/mainchannelorg3anchor.tx -channelID mainchannel -asOrg Org3MSP
+
+cp ./orderer/orderer.genesis.block crypto-config/ordererOrganizations/segfault.com/orderers/orderer0.segfault.com
+cp ./orderer/orderer.genesis.block crypto-config/ordererOrganizations/segfault.com/orderers/orderer1.segfault.com
+cp ./orderer/orderer.genesis.block crypto-config/ordererOrganizations/segfault.com/orderers/orderer2.segfault.com
+
+# mkdir -p /storage/orderer1
+# mkdir -p /storage/orderer2
+# mkdir -p /storage/orderer3
+
+# cp -R crypto-config/ordererOrganizations/segfault.com/orderers/orderer0.segfault.com /storage/orderer1
+# cp -R crypto-config/ordererOrganizations/segfault.com/orderers/orderer1.segfault.com /storage/orderer2
+# cp -R crypto-config/ordererOrganizations/segfault.com/orderers/orderer2.segfault.com /storage/orderer3
